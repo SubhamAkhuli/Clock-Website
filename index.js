2136756cc6952b2042ef2322f95ea7e9d9955674
Subham Akhuli <99539892+SubhamAkhuli@users.noreply.github.com>
2023-04-15 22:54:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,47 @@ setInterval(() => {
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
+    // To print the time and date
+    function formatTime(date) {
+        const hours12 = date.getHours() % 12 || 12;
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const isAm = date.getHours() < 12;
+
+        return `${hours12.toString().padStart(2, "0")}:${minutes
+            .toString()
+            .padStart(2, "0")}:${seconds
+            .toString()
+            .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
+    }
+    function formatDate(date) {
+        const DAYS = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+        ];
+        const MONTHS = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ];
+
+        return `${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]
+            } ${date.getDate()} ${date.getFullYear()}`;
+    }
+    timeElement.textContent = formatTime(d);
+    dateElement.textContent = formatDate(d);
 }, 1000);
